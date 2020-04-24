9c02da329c76b72635fe3f75e72956609bf027dc
Vinetos <Vinetos@users.noreply.github.com>
2020-04-24 14:46:25 +0000

    Deploying to master from  @ df4ba555e08c31284315556779d9808debe92731 🚀

---
+++ precache-manifest.c1eb61769095c29a96395e77bc48c42d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69565999f195294cc14a",
+    "url": "/css/app.2832622a.css"
+  },
+  {
+    "revision": "55d6bec438c5ae01f27e",
+    "url": "/css/infos.00910dc6.css"
+  },
+  {
+    "revision": "9c776c81718d9a93d07bbdba4822f3c0",
+    "url": "/img/logo.9c776c81.png"
+  },
+  {
+    "revision": "556eb2bfaa2f6d2fba9b53118f0a9fb2",
+    "url": "/img/screen.556eb2bf.png"
+  },
+  {
+    "revision": "ea9c807ecfc9548750b52a1315397607",
+    "url": "/img/screen2.ea9c807e.png"
+  },
+  {
+    "revision": "13c8e3eb40f319065f03a1bf77bbd818",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb30af80a05b5a08b9f4",
+    "url": "/js/about.c3cd0b24.js"
+  },
+  {
+    "revision": "69565999f195294cc14a",
+    "url": "/js/app.3f326b21.js"
+  },
+  {
+    "revision": "d6ec56633d1c73e6cc4a",
+    "url": "/js/chunk-vendors.584df8d9.js"
+  },
+  {
+    "revision": "55d6bec438c5ae01f27e",
+    "url": "/js/infos.3b0040d3.js"
+  },
+  {
+    "revision": "58f166d85610f0ecd1a535a77f4bff8d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.754db796b3933aac573aca00308f83b5.js"
+  "/precache-manifest.c1eb61769095c29a96395e77bc48c42d.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "safexty-website"});
